777b71668abc77dddb740c5a71996896780942f8
brettjm <brettjmanning@gmail.com>
2018-11-27 21:53:11 -0600

    fix formatting

--- monitor.py
+++ monitor.py
@@ -3,8 +3,9 @@ import picamera
 import numpy as np
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (320, 240)
-    camera.framerate = 24
-    time.sleep(2)
-    image = np.empty((240, 320, 3), dtype=np.uint8)
-    camera.capture(image, 'bgr')
+   camera.resolution = (320, 240)
+   camera.framerate = 24
+   time.sleep(2)
+   image = np.empty((240, 320, 3), dtype=np.uint8)
+   camera.capture(image, 'bgr')
+   
